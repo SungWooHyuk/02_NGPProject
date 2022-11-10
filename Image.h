d78211f6a2086004bbd2f07d33709af0fdde9770
SUNGWOOHYUK <tjddngur123@gmail.com>
2022-11-10 16:33:07 +0900

    InitData

---
+++ Image.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <atlimage.h>
+
+CImage PlayerImg;
+CImage BackGround;
+CImage FloorImg;   
+CImage ThronImg;
+CImage Fire;
+CImage Fire2;
+CImage PatternImg;
+CImage Pattern2;
+CImage Pattern3;
+CImage Pattern4;
+CImage Pattern5;
+
+//흑백이미지 
+CImage GS_Pattern;
+CImage GS_Pattern2;
+CImage GS_Pattern3;
+CImage GS_Pattern4;
+CImage GS_Pattern5;
+
+//문
+CImage CloseDoor;
+
+//gameover
+CImage GameOver;
+
+void Image()
+{
+	BackGround.Load(L"Image/background.png");
+	PlayerImg.Load(L"Image/Player.png");
+	FloorImg.Load(L"Image/floor.png"); 
+	ThronImg.Load(L"Image/thorn.png"); 
+	Fire.Load(L"Image/fire1.png");
+	Fire2.Load(L"Image/fire3.png");
+	PatternImg.Load(L"Image/pattern1.png"); 
+	Pattern2.Load(L"Image/pattern2.png");
+	Pattern3.Load(L"Image/pattern3.png");
+	Pattern4.Load(L"Image/pattern4.png");
+	Pattern5.Load(L"Image/pattern5.png");
+
+
+	GS_Pattern.Load(L"Image/gs_pattern1.png");
+	GS_Pattern2.Load(L"Image/gs_pattern2.png");
+	GS_Pattern3.Load(L"Image/gs_pattern3.png");
+	GS_Pattern4.Load(L"Image/gs_pattern4.png");
+	GS_Pattern5.Load(L"Image/gs_pattern5.png");
+
+	CloseDoor.Load(L"Image/closeDoor.png");
+	GameOver.Load(L"Image/GameOver.png");
+}
---
+++ Server.h
@@ -0,0 +1,18 @@
+#pragma once 
+#include "Global.h" 
+#include "protocol.h"
+#include "Functions.h"
+#include "Image.h" // 이미지 로드 헤더
+
+
+Player playerStatus; // 플레이어 상태  
+Object floorStatus[FLOORCNT]; // 바닥상태    
+Object FireStatus[FIRECNT]; // 불 상태       
+Object W_FireStatus[FIRECNT]; // 가로 불 상태
+Object ThornStatus[THORNCNT]; // 가시 상태 
+Object PatternStatus[PATTERNCNT]; // 패턴 상태 
+Object gs_PatternStatus[PATTERNCNT]; // 흑백 패턴 상태   
+Object doorstatus; //문의 상태      
+
+void State(); // 지금 모든 오브젝트들 상태를 이 함수에서 변경 
+void SettingObjLocation(); //초기 모든 objet들의 위치 설정 
